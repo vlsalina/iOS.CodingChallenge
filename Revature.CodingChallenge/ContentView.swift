92c04e6fbab7d4bfc663568f905b842baebf7dda
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-25 08:12:14 -0700

    First commit

---
+++ Revature.CodingChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Revature.CodingChallenge
+//
+//  Created by Vincent Salinas on 7/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Revature.CodingChallenge/Revature_CodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Revature_CodingChallengeApp.swift
+//  Revature.CodingChallenge
+//
+//  Created by Vincent Salinas on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Revature_CodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
