69804453ba1faa94a58fd8a40e4a90ac16b5cf27
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-25 13:12:42 -0700

    Created localization (EN and FR) files

--- Revature.CodingChallenge/RandomBreweryPage/Views/RandomBreweryPage.swift
+++ Revature.CodingChallenge/RandomBreweryPage/Views/RandomBreweryPage.swift
@@ -10,8 +10,6 @@ import SwiftUI
 struct RandomBreweryPage: View {
     @StateObject var services = Services()
     
-    //    @State var randomBrewery = ModelData(id: "", name: "", brewery_type: "", street: "", address_2: "", address_3: "", city: "", state: "", county_province: "", postal_code: "", country: "", longitude: "", latitude: "", phone: "", website_url: "", updated_at: "", created_at: "")
-    
     var body: some View {
         VStack(spacing: 5) {
             NavigationBar()
