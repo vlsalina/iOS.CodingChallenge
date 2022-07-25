f929808ca13286d4f110d4ada06900bb54a04de2
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-25 12:39:03 -0700

    Implemented random brewery fetch request functionality.

--- Revature.CodingChallenge/Services/Services.swift
+++ Revature.CodingChallenge/Services/Services.swift
@@ -9,6 +9,7 @@ import Foundation
 
 class Services: ObservableObject {
     @Published private(set) var breweries = [ModelData]()
+    @Published private(set) var randomBrewery = ModelData(id: "", name: "", brewery_type: "", street: "", address_2: "", address_3: "", city: "", state: "", county_province: "", postal_code: "", country: "", longitude: "", latitude: "", phone: "", website_url: "", updated_at: "", created_at: "")
     
     func getData() {
         let url = "https://mocki.io/v1/214223dc-c8f4-4a2f-9749-8ce2f355d04f"
@@ -33,6 +34,35 @@ class Services: ObservableObject {
             
             DispatchQueue.main.async {
                 self.breweries = jsonData
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func getRandomBrewery() {
+        let url = "https://api.openbrewerydb.org/breweries/random"
+        let session = URLSession.shared
+        let decoder = JSONDecoder()
+        guard let jsonURL = URL(string: url) else {
+            fatalError("Invalid URL")
+        }
+        
+        let task = session.dataTask(with: jsonURL) { data, response, error in
+            if let error = error {
+                fatalError(error.localizedDescription)
+            }
+            
+            guard let responseData = try? Data(contentsOf: jsonURL) else {
+                fatalError("Could not retrieve data")
+            }
+            
+            guard let jsonData = try? decoder.decode(ModelData.self, from: responseData) else {
+                fatalError("Could not decode data")
+            }
+            
+            DispatchQueue.main.async {
+                self.randomBrewery = jsonData
             }
         }
         
