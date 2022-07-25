0165d1c42c89496dacadda39fc68cec645d40d0f
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-25 08:32:45 -0700

    Created Services manager

--- Revature.CodingChallenge/Services/Services.swift
+++ Revature.CodingChallenge/Services/Services.swift
@@ -8,5 +8,35 @@
 import Foundation
 
 class Services: ObservableObject {
+    @Published private(set) var cities = [ModelData]()
+    
+    func getData() {
+        let url = "https://mocki.io/v1/214223dc-c8f4-4a2f-9749-8ce2f355d04f"
+        let session = URLSession.shared
+        let decoder = JSONDecoder()
+        guard let jsonURL = URL(string: url) else {
+            fatalError("Invalid URL")
+        }
+        
+        let task = session.dataTask(with: jsonURL) { data, response, error in
+            if let error = error {
+                fatalError(error.localizedDescription)
+            }
+            
+            guard let responseData = try? Data(contentsOf: jsonURL) else {
+                fatalError("Could not retrieve data")
+            }
+            
+            guard let jsonData = try? decoder.decode([ModelData].self, from: responseData) else {
+                fatalError("Could not decode data")
+            }
+            
+            DispatchQueue.main.async {
+                self.cities = jsonData
+            }
+        }
+        
+        task.resume()
+    }
     
 }
